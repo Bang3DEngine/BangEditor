bd772282c92afacbc6dd73c530ea424ccecc011a
sephirot47 <victorantondominguez@gmail.com>
2018-02-05 11:02:24 +0100

    EditorFileTracker

---
+++ include/BangEditor/EditorFileTracker.h
@@ -0,0 +1,50 @@
+#ifndef EDITORFILETRACKER_H
+#define EDITORFILETRACKER_H
+
+#include "Bang/Bang.h"
+#include "Bang/FileTracker.h"
+
+#include "BangEditor/BangEditor.h"
+#include "BangEditor/ProjectManager.h"
+
+NAMESPACE_BANG_BEGIN
+FORWARD class FileTracker;
+NAMESPACE_BANG_END
+
+USING_NAMESPACE_BANG
+NAMESPACE_BANG_EDITOR_BEGIN
+
+FORWARD class BehaviourTracker;
+
+class EditorFileTracker : public IFileTrackerListener,
+                          public IProjectManagerListener
+{
+public:
+	EditorFileTracker();
+	virtual ~EditorFileTracker();
+
+    FileTracker* GetFileTracker() const;
+    BehaviourTracker* GetBehaviourTracker() const;
+
+    void OnPathRenamed(const Path &previousPath, const Path &newPath);
+
+    static EditorFileTracker *GetInstance();
+
+private:
+    FileTracker *m_fileTracker = nullptr;
+    BehaviourTracker *m_behaviourFileTracker = nullptr;
+
+    // IFileTrackerListener
+    void OnPathAdded(const Path &addedPath) override;
+    void OnPathModified(const Path &modifiedPath) override;
+    void OnPathRemoved(const Path &removedPath) override;
+
+    // IProjectManagerListener
+    void OnProjectOpen(const Project *project) override;
+    void OnProjectClosed(const Project *project) override;
+};
+
+NAMESPACE_BANG_EDITOR_END
+
+#endif // EDITORFILETRACKER_H
+
---
+++ src/EditorFileTracker.cpp
@@ -0,0 +1,95 @@
+#include "BangEditor/EditorFileTracker.h"
+
+#include "Bang/File.h"
+#include "Bang/Paths.h"
+#include "Bang/Resources.h"
+#include "Bang/ImportFilesManager.h"
+
+#include "BangEditor/EditorScene.h"
+#include "BangEditor/BehaviourTracker.h"
+#include "BangEditor/EditorSceneManager.h"
+
+USING_NAMESPACE_BANG
+USING_NAMESPACE_BANG_EDITOR
+
+EditorFileTracker::EditorFileTracker()
+{
+    m_fileTracker = new FileTracker();
+    m_behaviourFileTracker = new BehaviourTracker();
+
+    GetFileTracker()->SetCheckFrequencySeconds(3.0f);
+    GetFileTracker()->TrackPath( Paths::GetEngineAssetsDir() );
+    GetFileTracker()->TrackPath( Paths::GetProjectAssetsDir() );
+
+    GetFileTracker()->
+            EventEmitter<IFileTrackerListener>::RegisterListener(this);
+    ProjectManager::GetInstance()->
+           EventEmitter<IProjectManagerListener>::RegisterListener(this);
+}
+
+EditorFileTracker::~EditorFileTracker()
+{
+    delete m_fileTracker;
+    delete m_behaviourFileTracker;
+}
+
+FileTracker* EditorFileTracker::GetFileTracker() const
+{
+    return m_fileTracker;
+}
+
+BehaviourTracker *EditorFileTracker::GetBehaviourTracker() const
+{
+    return m_behaviourFileTracker;
+}
+
+void EditorFileTracker::OnPathRenamed(const Path &previousPath, const Path &newPath)
+{
+    ImportFilesManager::OnFilepathRenamed(previousPath, newPath);
+}
+
+EditorFileTracker *EditorFileTracker::GetInstance()
+{
+    return EditorSceneManager::GetEditorScene()->GetEditorFileTracker();
+}
+
+void EditorFileTracker::OnPathAdded(const Path &addedPath)
+{
+    if (!ImportFilesManager::IsImportFile(addedPath) &&
+        !ImportFilesManager::HasImportFile(addedPath))
+    {
+        ImportFilesManager::CreateImportFile(addedPath);
+    }
+}
+
+void EditorFileTracker::OnPathModified(const Path &modifiedPath)
+{
+    if (ImportFilesManager::IsImportFile(modifiedPath)) { return; }
+
+    if (modifiedPath.IsFile())
+    {
+        // Refresh/reimport resources of the modified path
+        Array<Resource*> pathResources = Resources::GetCached(modifiedPath);
+        for (Resource *pathRes : pathResources)
+        {
+            Resources::Import(pathRes);
+        }
+    }
+}
+
+void EditorFileTracker::OnPathRemoved(const Path &removedPath)
+{
+    Path importPath = ImportFilesManager::GetImportFilepath(removedPath);
+    File::Remove(importPath);
+}
+
+void EditorFileTracker::OnProjectOpen(const Project*)
+{
+    GetFileTracker()->TrackPath( Paths::GetProjectAssetsDir() );
+}
+
+void EditorFileTracker::OnProjectClosed(const Project*)
+{
+    GetFileTracker()->UnTrackPath( Paths::GetProjectAssetsDir() );
+}
+
