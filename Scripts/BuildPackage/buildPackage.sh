6268d58edb7879fb1fa4ebc1a37b73f3ad61115d
sephirot47 <victorantondominguez@gmail.com>
2018-03-29 17:20:19 +0000

    Little change to buildPackage.sh

--- Scripts/BuildPackage/buildPackage.sh
+++ Scripts/BuildPackage/buildPackage.sh
@@ -5,6 +5,10 @@ cd "$(dirname "$0")"
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF ..
-make -j6
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
+make VERBOSE=1 -j6
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
 tar -czvf BangEditorPackage.tar.gz BangEditorPackage/
+if [ $? -ne 0 ] ; then exit 1 ; fi
