f66ff7214e2969e31d53735b1ae17f834465ad08
sephirot47 <victorantondominguez@gmail.com>
2018-04-04 11:03:19 +0200

    Added CMake options to buildPackage

--- Scripts/BuildPackage/buildPackage.sh
+++ Scripts/BuildPackage/buildPackage.sh
@@ -6,7 +6,7 @@ rm -rf BangEditor
 
 mkdir -p build
 cd build
-PATH=${PATH}:/usr/lib/x86_64-linux-gnu/ ; cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF ..
+PATH=${PATH}:/usr/lib/x86_64-linux-gnu/ ; cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF $1 ..
 if [ $? -ne 0 ] ; then exit 1 ; fi
 
 make VERBOSE=1 -j6
