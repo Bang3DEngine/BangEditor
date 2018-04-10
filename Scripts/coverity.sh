ddf0c1a64a768dd6f1302ac6ba497502cbcedc2b
sephirot47 <victorantondominguez@gmail.com>
2018-04-10 08:49:21 +0200

    Fixing build...

---
+++ Scripts/coverity.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd ..
+
+COV_TGZ="BangEditor_Coverity.tgz"
+TMP_COMPILE_FILE=".tmp.compile.sh"
+
+# Create build script
+echo -e "#!/bin/bash \n\n mkdir -p coverityBuild \n cd coverityBuild \n cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .. \n make -j6" > ${TMP_COMPILE_FILE}
+chmod +x ${TMP_COMPILE_FILE}
+
+# Build
+cov-build --dir cov-int ./${TMP_COMPILE_FILE}
+rm ${TMP_COMPILE_FILE}
+
+# Tar the result
+tar -czvf ${COV_TGZ} cov-int
+rm -rf cov-int
+
+# Upload:
+curl --form token=aFpZvYVCRnAJE4zuYkTKqw \
+     --form email=victorantondominguez@gmail.com \
+     --form file=@$(pwd)/${COV_TGZ} \
+     --form version="0.0" \
+     --form description="Automated_Coverity_Upload" \
+     https://scan.coverity.com/builds?project=Bang3DEngine%2FBangEditor
