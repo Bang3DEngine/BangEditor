70fb4276988c4156718442d8c1d1bfa880d9effb
sephirot47 <victorantondominguez@gmail.com>
2018-04-22 20:18:56 +0200

    Fix CMake stuff

--- Scripts/coverity.sh
+++ Scripts/coverity.sh
@@ -7,7 +7,7 @@ COV_TGZ="BangEditor_Coverity.tgz"
 TMP_COMPILE_FILE=".tmp.compile.sh"
 
 # Create build script
-echo -e "#!/bin/bash \n\n mkdir -p coverityBuild \n cd coverityBuild \n cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .. \n make -j6" > ${TMP_COMPILE_FILE}
+echo -e "#!/bin/bash \n\n mkdir -p buildCoverity \n cd buildCoverity \n cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .. \n make -j6" > ${TMP_COMPILE_FILE}
 chmod +x ${TMP_COMPILE_FILE}
 
 # Build
@@ -19,9 +19,10 @@ tar -czvf ${COV_TGZ} cov-int
 rm -rf cov-int
 
 # Upload:
-curl --form token=aFpZvYVCRnAJE4zuYkTKqw \
-     --form email=victorantondominguez@gmail.com \
-     --form file=@$(pwd)/${COV_TGZ} \
-     --form version="0.0" \
-     --form description="Automated_Coverity_Upload" \
-     https://scan.coverity.com/builds?project=Bang3DEngine%2FBangEditor
+#curl --form token=aFpZvYVCRnAJE4zuYkTKqw \
+#     --form email=victorantondominguez@gmail.com \
+#     --form file=@$(pwd)/${COV_TGZ} \
+#     --form version="0.0" \
+#     --form description="Automated_Coverity_Upload" \
+#     https://scan.coverity.com/builds?project=Bang3DEngine%2FBangEditor
+
